f3a6f3956cbab511b6d5f6da03ba9bf1ea6895a6
yeyel <arielglvz@gmail.com>
2024-06-05 00:45:02 +0800

    feat: added ui

---
+++ src/components/main-content/components/navigation/index.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "./navigation.scss"
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Resume</li>
+        <li>Portfolio</li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
--- src/components/main-content/index.tsx
+++ src/components/main-content/index.tsx
@@ -1,8 +1,18 @@
 import React from "react"
 import "./main-content..scss"
+import Navigation from "./components/navigation"
 
 const MainContent = () => {
-  return <div className="mainContent">MainContent</div>
+  return (
+    <div className="mainContent">
+      <div className="header">
+        <div className="title">
+          <h1>About</h1>
+        </div>
+        <Navigation />
+      </div>
+    </div>
+  )
 }
 
 export default MainContent
